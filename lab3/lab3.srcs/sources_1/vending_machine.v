775358e23d371d9fe83e7b56843257df0510df9b
Ik Hwan Kim <12kimih@gmail.com>
2022-03-26 20:15:39 +0900

    improve branch tree

--- lab3/lab3.srcs/sources_1/vending_machine.v
+++ lab3/lab3.srcs/sources_1/vending_machine.v
@@ -46,7 +46,7 @@ module vending_machine(
     // Combinational circuit
     always @(*) begin
         output_item = i_select_item & o_available_item;
-        if (i_input_coin != `kNumCoins'b0) begin
+        if (|i_input_coin) begin
             case (i_input_coin)
                 `kNumCoins'b1: current_total = o_current_total + kkCoinValue[0];
                 `kNumCoins'b10: current_total = o_current_total + kkCoinValue[1];
@@ -54,7 +54,7 @@ module vending_machine(
                 default: current_total = o_current_total;
             endcase
         end
-        else if (output_item != `kNumItems'b0) begin
+        else if (|output_item) begin
             case (output_item)
                 `kNumItems'b1: current_total = o_current_total - kkItemPrice[0];
                 `kNumItems'b10: current_total = o_current_total - kkItemPrice[1];
@@ -91,70 +91,69 @@ module vending_machine(
                              (current_total >= kkItemPrice[0]) ? `kNumItems'b1 : `kNumItems'b0;
             o_output_item <= output_item;
             o_current_total <= current_total;
-            if (i_trigger_return) begin
+            if (|i_input_coin) begin
+                o_return_coin <= `kReturnCoins'b0;
+                case (i_input_coin)
+                    `kNumCoins'b1:
+                        if (num_coins[0] == `kCoinBits'd4 && num_coins[1] == `kCoinBits'd1) begin
+                            num_coins[0] <= `kCoinBits'd0;
+                            num_coins[1] <= `kCoinBits'd0;
+                            num_coins[2] <= num_coins[2] + `kCoinBits'd1;
+                        end
+                        else if (num_coins[0] == `kCoinBits'd4) begin
+                            num_coins[0] <= `kCoinBits'd0;
+                            num_coins[1] <= num_coins[1] + `kCoinBits'd1;
+                        end
+                        else begin
+                            num_coins[0] <= num_coins[0] + `kCoinBits'd1;
+                        end
+                    `kNumCoins'b10:
+                        if (num_coins[1] == `kCoinBits'd1) begin
+                            num_coins[1] <= `kCoinBits'd0;
+                            num_coins[2] <= num_coins[2] + `kCoinBits'd1;
+                        end
+                        else begin
+                            num_coins[1] <= num_coins[1] + `kCoinBits'd1;
+                        end
+                    `kNumCoins'b100:
+                        num_coins[2] <= num_coins[2] + `kCoinBits'd1;
+                endcase
+            end
+            else if (|output_item) begin
+                o_return_coin <= `kReturnCoins'b0;
+                case (output_item)
+                    `kNumItems'b1:
+                        if (num_coins[0] < `kCoinBits'd4 && num_coins[1] == `kCoinBits'd0) begin
+                            num_coins[0] <= num_coins[0] + `kCoinBits'd5 - `kCoinBits'd4;
+                            num_coins[1] <= num_coins[1] + `kCoinBits'd2 - `kCoinBits'd1;
+                            num_coins[2] <= num_coins[2] - `kCoinBits'd1;
+                        end
+                        else if (num_coins[0] < `kCoinBits'd4) begin
+                            num_coins[0] <= num_coins[0] + `kCoinBits'd5 - `kCoinBits'd4;
+                            num_coins[1] <= num_coins[1] - `kCoinBits'd1;
+                        end
+                        else begin
+                            num_coins[0] <= num_coins[0] - `kCoinBits'd4;
+                        end
+                    `kNumItems'b10:
+                        if (num_coins[1] < `kCoinBits'd1) begin
+                            num_coins[1] <= num_coins[1] + `kCoinBits'd2 - `kCoinBits'd1;
+                            num_coins[2] <= num_coins[2] - `kCoinBits'd1;
+                        end
+                        else begin
+                            num_coins[1] <= num_coins[1] - `kCoinBits'd1;
+                        end
+                    `kNumItems'b100:
+                        num_coins[2] <= num_coins[2] - `kCoinBits'd1;
+                    `kNumItems'b1000:
+                        num_coins[2] <= num_coins[2] - `kCoinBits'd2;
+                endcase
+            end
+            else if (i_trigger_return) begin
                 o_return_coin <= num_coins[0] + num_coins[1] + num_coins[2];
                 num_coins[0] <= `kCoinBits'b0;
                 num_coins[1] <= `kCoinBits'b0;
                 num_coins[2] <= `kCoinBits'b0;
-            end
-            else begin
-                o_return_coin <= `kReturnCoins'b0;
-                if (i_input_coin != `kNumCoins'b0) begin
-                    case (i_input_coin)
-                        `kNumCoins'b1:
-                            if (num_coins[0] == `kCoinBits'd4 && num_coins[1] == `kCoinBits'd1) begin
-                                num_coins[0] <= `kCoinBits'd0;
-                                num_coins[1] <= `kCoinBits'd0;
-                                num_coins[2] <= num_coins[2] + `kCoinBits'd1;
-                            end
-                            else if (num_coins[0] == `kCoinBits'd4) begin
-                                num_coins[0] <= `kCoinBits'd0;
-                                num_coins[1] <= num_coins[1] + `kCoinBits'd1;
-                            end
-                            else begin
-                                num_coins[0] <= num_coins[0] + `kCoinBits'd1;
-                            end
-                        `kNumCoins'b10:
-                            if (num_coins[1] == `kCoinBits'd1) begin
-                                num_coins[1] <= `kCoinBits'd0;
-                                num_coins[2] <= num_coins[2] + `kCoinBits'd1;
-                            end
-                            else begin
-                                num_coins[1] <= num_coins[1] + `kCoinBits'd1;
-                            end
-                        `kNumCoins'b100:
-                            num_coins[2] <= num_coins[2] + `kCoinBits'd1;
-                    endcase
-                end
-                else if (output_item != `kNumItems'b0) begin
-                    case (output_item)
-                        `kNumItems'b1:
-                            if (num_coins[0] < `kCoinBits'd4 && num_coins[1] == `kCoinBits'd0) begin
-                                num_coins[0] <= num_coins[0] + `kCoinBits'd5 - `kCoinBits'd4;
-                                num_coins[1] <= num_coins[1] + `kCoinBits'd2 - `kCoinBits'd1;
-                                num_coins[2] <= num_coins[2] - `kCoinBits'd1;
-                            end
-                            else if (num_coins[0] < `kCoinBits'd4) begin
-                                num_coins[0] <= num_coins[0] + `kCoinBits'd5 - `kCoinBits'd4;
-                                num_coins[1] <= num_coins[1] - `kCoinBits'd1;
-                            end
-                            else begin
-                                num_coins[0] <= num_coins[0] - `kCoinBits'd4;
-                            end
-                        `kNumItems'b10:
-                            if (num_coins[1] < `kCoinBits'd1) begin
-                                num_coins[1] <= num_coins[1] + `kCoinBits'd2 - `kCoinBits'd1;
-                                num_coins[2] <= num_coins[2] - `kCoinBits'd1;
-                            end
-                            else begin
-                                num_coins[1] <= num_coins[1] - `kCoinBits'd1;
-                            end
-                        `kNumItems'b100:
-                            num_coins[2] <= num_coins[2] - `kCoinBits'd1;
-                        `kNumItems'b1000:
-                            num_coins[2] <= num_coins[2] - `kCoinBits'd2;
-                    endcase
-                end
             end
         end
     end
