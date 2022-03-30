cb172f5cc5f7c33f16610ce72eb4c3d5a175f364
Ik Hwan Kim <12kimih@gmail.com>
2022-03-31 01:28:44 +0900

    case using macros

--- lab1/lab1.srcs/ALU.v
+++ lab1/lab1.srcs/ALU.v
@@ -33,22 +33,22 @@ module ALU (
 
     always @(*) begin
         case (OP)
-            4'b0000: {Cout, C} = A + B + Cin;
-            4'b0001: {Cout, C} = A - (B + Cin);
-            4'b0010: {Cout, C} = {1'b0, A};
-            4'b0011: {Cout, C} = {1'b0, ~(A & B)};
-            4'b0100: {Cout, C} = {1'b0, ~(A | B)};
-            4'b0101: {Cout, C} = {1'b0, ~(A ^ B)};
-            4'b0110: {Cout, C} = {1'b0, ~A};
-            4'b0111: {Cout, C} = {1'b0, A & B};
-            4'b1000: {Cout, C} = {1'b0, A | B};
-            4'b1001: {Cout, C} = {1'b0, A ^ B};
-            4'b1010: {Cout, C} = {1'b0, 1'b0, A[15:1]};
-            4'b1011: {Cout, C} = {1'b0, A[15], A[15:1]};
-            4'b1100: {Cout, C} = {1'b0, A[0], A[15:1]};
-            4'b1101: {Cout, C} = {1'b0, A[14:0], 1'b0};
-            4'b1110: {Cout, C} = {1'b0, A[14:0], 1'b0};
-            4'b1111: {Cout, C} = {1'b0, A[14:0], A[15]};
+            `OP_ADD: {Cout, C} = A + B + Cin;
+            `OP_SUB: {Cout, C} = A - (B + Cin);
+            `OP_ID: {Cout, C} = {1'b0, A};
+            `OP_NAND: {Cout, C} = {1'b0, ~(A & B)};
+            `OP_NOR: {Cout, C} = {1'b0, ~(A | B)};
+            `OP_XNOR: {Cout, C} = {1'b0, ~(A ^ B)};
+            `OP_NOT: {Cout, C} = {1'b0, ~A};
+            `OP_AND: {Cout, C} = {1'b0, A & B};
+            `OP_OR: {Cout, C} = {1'b0, A | B};
+            `OP_XOR: {Cout, C} = {1'b0, A ^ B};
+            `OP_LRS: {Cout, C} = {1'b0, 1'b0, A[15:1]};
+            `OP_ARS: {Cout, C} = {1'b0, A[15], A[15:1]};
+            `OP_RR: {Cout, C} = {1'b0, A[0], A[15:1]};
+            `OP_LLS: {Cout, C} = {1'b0, A[14:0], 1'b0};
+            `OP_ALS: {Cout, C} = {1'b0, A[14:0], 1'b0};
+            `OP_RL: {Cout, C} = {1'b0, A[14:0], A[15]};
         endcase
     end
 endmodule
